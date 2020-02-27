52961953fb53b24bcb99fcb871ad8ae6613622e6
Robert Ragas <robertragas@goalgorilla.com>
2020-02-27 14:04:06 +0100

    by robertragas: add custom rule action to send out a private message

---
+++ modules/social_features/social_private_message/src/Plugin/RulesAction/SendPrivateMessage.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Drupal\social_private_message\Plugin\RulesAction;
+
+use Drupal\rules\Core\RulesActionBase;
+use Drupal\user\UserDataInterface;
+
+/**
+ * Provides a 'Send Private Message' action.
+ *
+ * @RulesAction(
+ *   id = "rules_send_private_message",
+ *   label = @Translation("Send private message"),
+ *   category = @Translation("Open Social"),
+ *   context = {
+ *     "userid" = @ContextDefinition("integer",
+ *       label = @Translation("The user ID"),
+ *       description = @Translation("Which User ID should be the sender")
+ *     ),
+ *     "message" = @ContextDefinition("string",
+ *       label = @Translation("Message content"),
+ *       description = @Translation("The private message content to send to the user")
+ *     ),
+ *   }
+ * )
+ */
+class SendPrivateMessage extends RulesActionBase {
+
+  /**
+   * Send a private message.
+   */
+  protected function doExecute() {
+    $sender = \Drupal\user\Entity\User::load($this->getContextValue('userid'));
+    $recipients[] = $sender;
+
+    $receiver = \Drupal\user\Entity\User::load(\Drupal::currentUser()->id());
+    $recipients[] = $receiver;
+
+    /** @var \Drupal\private_message\Service\PrivateMessageServiceInterface $private_message_service */
+    $private_message_service = \Drupal::service('private_message.service');
+
+    // Create a pm thread between these users.
+    $thread = $private_message_service->getThreadForMembers($recipients);
+
+    // Get body of pm.
+    $private_message_body = check_markup($this->getContextValue('message'), 'plain_text');
+
+    // Create a single message with the pm body.
+    $private_message = \Drupal::entityTypeManager()->getStorage('private_message')->create([
+      'owner' => $sender,
+      'message' => $private_message_body,
+    ]);
+
+    $private_message->save();
+    $thread->addMessage($private_message)->save();
+
+    // There is a contrib private message bug that when creating a new thread
+    // and adding messages to it, for the recipient the $last_message and
+    // $thread_last_check get the same timestamp. Showing no new messages badge.
+    // https://www.drupal.org/project/private_message/issues/3043898
+    // TODO:: Update to the correct version when issue has been solved.
+    /** @var UserDataInterface $userData */
+    $userData = \Drupal::service('user.data');
+    $userData->set('private_message', $receiver->id(), 'private_message_thread:' . $thread->id(), 0);
+  }
+
+}
