a1f0c31fec9d1823f37491c4d7be536d9a36bc66
Alexander Varwijk <alexander@getopensocial.com>
2020-03-20 01:23:44 +0100

    Provide social_search_comments module
    This contains a configuration override that will add the comments for
    nodes to the search indices and search views using the newly created
    teaser view mode. Enabling the module and re-indexing after clearing the
    cache should be enough.
    
    If that's not enough you may need to change something insignificant in
    an index and resave to retrigger the index caching as we've had to do
    with overrides for search (e.g. SOLR or content tagging) previously.

---
+++ modules/social_features/social_search/modules/social_search_comments/src/CommentConfigOverride.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace Drupal\social_search_comments;
+
+use Drupal\Core\Cache\CacheableMetadata;
+use Drupal\Core\Config\ConfigFactoryOverrideInterface;
+use Drupal\Core\Config\StorageInterface;
+
+/**
+ * Class CommentConfigOverride.
+ *
+ * Adds comments to the search indexes.
+ *
+ * @package Drupal\social_search_comments
+ */
+class CommentConfigOverride implements ConfigFactoryOverrideInterface {
+
+  /**
+   * Load overrides.
+   */
+  public function loadOverrides($names) {
+    $overrides = [];
+
+    // Thankfully the changes for all indexes are the same to add comments.
+    $indices = [
+      'search_api.index.social_content',
+      'search_api.index.social_all',
+    ];
+    foreach ($indices as $index) {
+      if (in_array($index, $names)) {
+        $overrides[$index] = [
+          'dependencies' => ['comment' => 'comment'],
+          'config' => ['field.storage.comment.field_comment_body' => 'field.storage.comment.field_comment_body'],
+          'field_settings' => [
+            'field_comment_body' => [
+              'label' => 'Comment',
+              'datasource_id' => 'entity:comment',
+              'property_path' => 'field_comment_body',
+              'type' => 'text',
+              'dependencies' => [
+                'config' => [
+                  'field.storage.comment.field_comment_body',
+                ],
+              ],
+            ],
+            'status_1' => [
+              'label' => 'status_1',
+              'datasource_id' => 'entity:comment',
+              'property_path' => 'status',
+              'type' => 'boolean',
+              'indexed_locked' => TRUE,
+              'type_locked' => TRUE,
+              'dependencies' => [
+                'module' => [
+                  'comment',
+                ],
+              ],
+            ],
+          ],
+          'datasource_settings' => [
+            'entity:comment' => [
+              'bundles' => [
+                'default' => TRUE,
+                'selected' => [
+                  'post_comment',
+                ],
+              ],
+              'languages' => [
+                'default' => TRUE,
+                'selected' => [],
+              ],
+            ],
+          ],
+          // List fields as key => value because we're deep merging and not just
+          // providing totally new arrays.
+          'processor_settings' => [
+            'ignorecase' => [
+              'fields' => [
+                'field_comment_body' => 'field_comment_body',
+              ],
+            ],
+            'tokenizer' => [
+              'fields' => [
+                'field_comment_body' => 'field_comment_body',
+              ],
+            ],
+            'transliteration' => [
+              'fields' => [
+                'field_comment_body' => 'field_comment_body',
+              ],
+            ],
+          ],
+        ];
+      }
+    }
+
+    // Thankfully the changes for both views are the same.
+    $views = [
+      'views.view.search_all',
+      'views.view.search_content',
+    ];
+    foreach ($views as $view) {
+      if (in_array($view, $names)) {
+        $overrides[$view] = [
+          'display' => [
+            'options' => [
+              'view_modes' => [
+                'entity:comment' => [
+                  'comment' => 'teaser',
+                  'post_comment' => 'teaser',
+                ],
+              ],
+            ],
+          ],
+        ];
+      }
+    }
+
+    return $overrides;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheSuffix() {
+    return 'SocialCommentConfigOverride';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheableMetadata($name) {
+    return new CacheableMetadata();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function createConfigObject($name, $collection = StorageInterface::DEFAULT_COLLECTION) {
+    return NULL;
+  }
+
+}
